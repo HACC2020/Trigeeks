66f03bdf5d9269197597c7387421ca984690b678
heweiron <42639451+heweiron@users.noreply.github.com>
2020-10-28 20:06:54 -1000

    finish login system and connecting to Firebase

---
+++ UHShield/UHShield/HelperClass/Modifiers.swift
@@ -0,0 +1,116 @@
+//
+//  Modifiers.swift
+//  UHShield
+//
+//  Created by weirong he on 10/28/20.
+//
+
+import SwiftUI
+
+struct Modifiers: View {
+    var body: some View {
+        VStack {
+            Button(action: {}, label: {
+                Text("Login")
+                    .fontWeight(.semibold)
+                    .font(.title2)
+                    .foregroundColor(Color(#colorLiteral(red: 0.8864660859, green: 0.8863860965, blue: 0.9189570546, alpha: 1)))
+            })
+            .buttonStyle(LongButtonStyle())
+        }
+    }
+}
+
+struct Modifiers_Previews: PreviewProvider {
+    static var previews: some View {
+        Modifiers()
+    }
+}
+
+struct TextFieldModifier: ViewModifier {
+    
+    func body(content: Content) -> some View {
+        content
+            .padding()
+            .background(
+                RoundedRectangle(cornerRadius: 15).foregroundColor(Color(#colorLiteral(red: 0.8864660859, green: 0.8863860965, blue: 0.9189570546, alpha: 1)))
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 15)
+                            .stroke(Color.gray, lineWidth: 4)
+                            .foregroundColor(Color(#colorLiteral(red: 0.8864660859, green: 0.8863860965, blue: 0.9189570546, alpha: 1)))
+                            .blur(radius: 4)
+                            .offset(x: 2, y: 4)
+                            .mask(RoundedRectangle(cornerRadius: 15).fill(LinearGradient(gradient: Gradient(colors: [Color.black, Color.clear]), startPoint: .topLeading, endPoint: .bottomTrailing)))
+                        
+                    )
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 15)
+                            .stroke(Color.white, lineWidth: 4)
+                            .foregroundColor(Color(#colorLiteral(red: 0.8864660859, green: 0.8863860965, blue: 0.9189570546, alpha: 1)))
+                            .blur(radius: 4)
+                            .offset(x: -2, y: -3)
+                            .mask(RoundedRectangle(cornerRadius: 15).fill(LinearGradient(gradient: Gradient(colors: [Color.clear, Color.white]), startPoint: .topLeading, endPoint: .bottomTrailing)))
+
+                    )
+            )
+    }
+    
+    
+}
+
+
+struct SectionModifier: ViewModifier {
+    
+    func body(content: Content) -> some View {
+        content
+            .background(
+                RoundedRectangle(cornerRadius: 15).foregroundColor(Color(#colorLiteral(red: 0.8864660859, green: 0.8863860965, blue: 0.9189570546, alpha: 1)))
+                    .shadow(color: Color.black.opacity(0.2), radius: 10, x: 5, y: 5)
+                    .shadow(color: Color.white.opacity(0.7), radius: 10, x: -5, y: -5)
+            )
+    }
+    
+    
+}
+
+struct LongButtonStyle: ButtonStyle {
+    var buttonColor = Color(#colorLiteral(red: 0.009583513252, green: 0.2368507981, blue: 0.6169182062, alpha: 1))
+    func makeBody(configuration: Self.Configuration) -> some View {
+        configuration.label
+            .padding()
+            .frame(maxWidth: .infinity)
+            .background(
+                Group {
+                    if configuration.isPressed {
+                    RoundedRectangle(cornerRadius: 15).fill(buttonColor)
+                        .shadow(color: Color(#colorLiteral(red: 0.2986346781, green: 0.2874504924, blue: 0.3008843064, alpha: 1)), radius: 5, x: 0, y: 0)
+                        .shadow(color: Color.white.opacity(0.7), radius: 10, x: 5, y: 5)
+                    } else {
+                        RoundedRectangle(cornerRadius: 15).foregroundColor(buttonColor)
+                            .shadow(color: Color(#colorLiteral(red: 0.2986346781, green: 0.2874504924, blue: 0.3008843064, alpha: 1)), radius: 5, x: 5, y: 8)
+                            .shadow(color: Color.white.opacity(0.7), radius: 10, x: -5, y: -5)
+                    }
+                }
+            )
+    }
+}
+
+struct SmallButtonStyle: ButtonStyle {
+    func makeBody(configuration: Self.Configuration) -> some View {
+        configuration.label
+            .padding()
+            .background(
+                Group {
+                    if configuration.isPressed {
+                    RoundedRectangle(cornerRadius: 15).fill(Color(#colorLiteral(red: 0.8864660859, green: 0.8863860965, blue: 0.9189570546, alpha: 1)))
+                        .shadow(color: Color.black.opacity(0.2), radius: 10, x: -5, y: -5)
+                        .shadow(color: Color.white.opacity(0.7), radius: 10, x: 5, y: 5)
+                    } else {
+                        RoundedRectangle(cornerRadius: 15).fill(Color(#colorLiteral(red: 0.8864660859, green: 0.8863860965, blue: 0.9189570546, alpha: 1)))
+                            .shadow(color: Color.black.opacity(0.2), radius: 10, x: 5, y: 5)
+                            .shadow(color: Color.white.opacity(0.7), radius: 10, x: -5, y: -5)
+                    }
+                }
+            )
+    }
+}
--- UHShield/UHShield/UHShieldApp.swift
+++ UHShield/UHShield/UHShieldApp.swift
@@ -6,12 +6,26 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct UHShieldApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView().environmentObject(SessionStore())
         }
+    }
+}
+
+
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        FirebaseApp.configure()
+        return true
     }
 }

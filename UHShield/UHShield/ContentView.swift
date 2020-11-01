032b023ba1969f8625ea37927fddaca64be899f6
tianhuizhou <tianhuizhou@users.noreply.github.com>
2020-10-31 16:00:27 -1000

    set up
    need to modify the logo

--- UHShield/UHShield/ContentView.swift
+++ UHShield/UHShield/ContentView.swift
@@ -9,7 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     @EnvironmentObject var session: SessionStore
+    @State private var showView = false
+    @State private var angle: Double = 720
+    @State private var opacity: Double = 1
+    @State private var scale: CGFloat = 1
+    
     var body: some View {
+        Group{
+            if showView{
         Group{
             
             if session.session != nil {
@@ -22,8 +29,22 @@ struct ContentView: View {
         }.onAppear {
             self.getUser()
         }
+            } else {
+                ZStack{
+                    Image("logo").rotation3DEffect(.degrees(angle), axis: (x: 0.0, y: 1.0, z: 0.0)).opacity(opacity).scaleEffect(scale)
+                }.onAppear{
+                    withAnimation(.linear(duration: 2)){
+                        angle = 0
+                        scale = 1
+                        opacity = 0
+                    }
+                    withAnimation(Animation.linear.delay(1.5)){
+                        showView = true
+                    }
+                }
+            }
     }
-    
+    }
     func getUser(){
         session.listen()
     }

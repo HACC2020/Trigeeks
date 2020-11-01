e7e771ccef75c3c990bb36ae50d9ac142a622448
tianhuizhou <tianhuizhou@users.noreply.github.com>
2020-10-31 21:51:02 -1000

    initialize files

---
+++ UHShield/UHShield/Views/SubView/BadgeRowView.swift
@@ -0,0 +1,20 @@
+//
+//  BadgeRowView.swift
+//  UHShield
+//
+//  Created by Tianhui Zhou on 10/31/20.
+//
+
+import SwiftUI
+
+struct BadgeRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BadgeRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        BadgeRowView()
+    }
+}

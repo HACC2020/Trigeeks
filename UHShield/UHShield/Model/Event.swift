9d1cbf6e020514b73f1b31392286e5282a870d79
heweiron <42639451+heweiron@users.noreply.github.com>
2020-10-31 22:19:08 -1000

    create event model and ViewModel

---
+++ UHShield/UHShield/Model/Event.swift
@@ -0,0 +1,36 @@
+//
+//  Event.swift
+//  UHShield
+//
+//  Created by weirong he on 10/31/20.
+//
+
+import Foundation
+import FirebaseFirestoreSwift
+
+struct Event: Codable, Identifiable {
+
+    @DocumentID var id: String? = UUID().uuidString
+    var sponsor: String
+    var guests: [String]
+    var arrivedGuests: [String]
+    var location: Location
+    var startTime: String
+    var endTime: String
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case sponsor
+        case guests
+        case arrivedGuests
+        case location
+        case startTime
+        case endTime
+    }
+    
+}
+
+struct Location: Codable {
+    var building: String
+    var roomID: Int
+}

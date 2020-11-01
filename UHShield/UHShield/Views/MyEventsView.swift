c20c1efbe207e465296e3b318eedce1ffc15a185
Yuhan Jiang <jiangyuh@hawaii.edu>
2020-10-31 20:09:31 -1000

    sponsor ui

---
+++ UHShield/UHShield/Views/MyEventsView.swift
@@ -0,0 +1,20 @@
+//
+//  MyEventsView.swift
+//  UHShield
+//
+//  Created by Yuhan Jiang on 2020/10/31.
+//
+
+import SwiftUI
+
+struct MyEventsView: View {
+    var body: some View {
+        Text("A list of events created by this sponsor")
+    }
+}
+
+struct MyEventsView_Previews: PreviewProvider {
+    static var previews: some View {
+        MyEventsView()
+    }
+}
